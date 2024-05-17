9077e7c8f90d3e8d68815159f192588e6069b9c3
HoangDuc020428 <113301177+HoangDuc020428@users.noreply.github.com>
2024-05-18 01:35:53 +0700

    Update Gaussian.m

--- Gaussian.m
+++ Gaussian.m
@@ -1,11 +1,9 @@
-% Kích th??c c?a ma tr?n
-rows = 10; % S? hàng
-cols = 10; % S? c?t
+% size of the matrix
+rows = 10; 
+cols = 10;
 
-% T?o ma tr?n ng?u nhiên theo phân ph?i Gaussian
-M = rows * cols; % T?ng s? ph?n t?
-sigma = 1 / sqrt(M); % ?? l?ch chu?n 
-random_matrix = sigma * randn(rows, cols); % T?o ma tr?n ng?u nhiên
+M = rows * cols; 
+sigma = 1 / sqrt(M); 
+random_matrix = sigma * randn(rows, cols); 
 
-% In ma tr?n ra màn hình
 disp(random_matrix);
